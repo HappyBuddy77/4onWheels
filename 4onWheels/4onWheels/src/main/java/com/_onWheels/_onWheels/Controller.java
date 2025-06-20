7ed6c874ca8229ecb4cc2c6aef22aeebe6fb3e94
Nguyen-Mary <52886817+HappyBuddy77@users.noreply.github.com>
2025-06-20 17:28:21 -0400

    Pt.2 Backbone code for: Controller Class Security Configuration Login View Page

---
+++ 4onWheels/4onWheels/src/main/java/com/_onWheels/_onWheels/Controller.java
@@ -0,0 +1,17 @@
+package com._onWheels._onWheels;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Controller implements WebMvcConfigurer {
+
+        @Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/HomePage").setViewName("HomePage");
+		registry.addViewController("/").setViewName("HomePage");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
