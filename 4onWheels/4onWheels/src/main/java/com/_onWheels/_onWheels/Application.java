3e4f4fa0ee88aedc145612f5790d6bba06b19a98
Nguyen-Mary <52886817+HappyBuddy77@users.noreply.github.com>
2025-07-03 17:27:13 -0400

    Merge with HomePage
    Working Merge with HomePage Branch

--- 4onWheels/4onWheels/src/main/java/com/_onWheels/_onWheels/Application.java
+++ 4onWheels/4onWheels/src/main/java/com/_onWheels/_onWheels/Application.java
@@ -2,13 +2,12 @@ package com._onWheels._onWheels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 // @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
-@ComponentScan(basePackages = {"com._onWheels._onWheels"})
+// @ComponentScan(basePackages = {"com._onWheels._onWheels"})
 public class Application {
 
 	public static void main(String[] args) {
