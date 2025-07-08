a6a291598b62f6d8a409bb995ad9ff574d69ee67
Nguyen-Mary <52886817+HappyBuddy77@users.noreply.github.com>
2025-07-08 18:45:15 -0400

    Checkout and EV Page
    - Add Payment Details UI (Need make spacing between border better)
    - Add functionality Remove items from Cart to Cart Details
    - Add Functionality to Cart Icon in header
    - Add Loan Calculator Button (Does nothing right now)

--- 4onWheels/4onWheels/src/main/resources/static/js/cartInputs.js
+++ 4onWheels/4onWheels/src/main/resources/static/js/cartInputs.js
@@ -18,13 +18,23 @@ function updateCartTotal() {
     for (let i = 1; i < rows.length - 1; i++) {
         const cells = rows[i].getElementsByTagName("td");
         const priceText = cells[3].innerText;
-        console.log("Price Text found: ", priceText);
+        // console.log("Price Text found: ", priceText);
         const price = parseFloat(priceText.replace('$', '').trim());
-        console.log("Price found: ", price);
+        // console.log("Price found: ", price);
         total += price;
     }
 
     // Update the total price in the last row
     console.log("total found: ", total);
     document.getElementById("total-price").textContent = "$" + total.toFixed(2);
+}
+function deleteItem(event) {
+    const isConfirmed = confirm("Are you sure you want to delete this item from your cart?");
+    if (isConfirmed) {
+        const row = event.target.closest('tr');
+        row.remove();
+    }
+    setTimeout(() => {
+            updateCartTotal();
+    }, 100); // for now keep at 100 milliseconds
 }
